6a6c01d0e6f9c9baefe21bfb86542d5150094509
DenysBespalko231452 <231452@buas.nl>
2025-01-16 19:05:36 +0100

    prepared for hpt

--- Y2B-2023-OT2_Twin-main/ot2-hpt.py
+++ Y2B-2023-OT2_Twin-main/ot2-hpt.py
@@ -3,8 +3,8 @@ from stable_baselines3 import PPO
 import os
 import wandb
 from stable_baselines3.common.callbacks import BaseCallback
-from clearml import Task
 from ot2_wrapper_final import OT2Env
+from clearml import Task
 
 task = Task.init(project_name="Mentor Group E/Group DMRM", task_name="ppo-hpt")
 
@@ -14,14 +14,13 @@ task.set_base_docker('deanis/2023y2b-rl:latest')
 #setting the task to run remotely on the default queue
 task.execute_remotely(queue_name="default")
 
-
 # Define sweep config
 sweep_config = {
     "method": "bayes",
     "name": "sweep",
     "metric": {"goal": "minimize", "name": "rollout/ep_len_mean"},
     "parameters": {
-        "learning_rate": {"distribution": "uniform", "min": 0.0003, "max": 0.00005},
+        "learning_rate": {"distribution": "uniform", "min": 0.3, "max": 0.8},
         # "n_steps": {"distribution": "int_uniform", "min": 128, "max": 512},
         # "batch_size": {"distribution": "int_uniform", "min": 32, "max": 256},
         # "gamma": {"distribution": "uniform", "min": 0.9, "max": 0.999},
@@ -35,19 +34,18 @@ def main(config=None):
 
     config = run.config
 
-    learning_rate = config.learning_rate if config.learning_rate is not None else 0.0003
-    n_steps = config.n_steps if config.n_steps is not None else 2048
-    batch_size = config.batch_size if config.batch_size is not None else 64
-    gamma = config.gamma if config.gamma is not None else 0.99
+    learning_rate = config.learning_rate 
+    # n_steps = config.n_steps 
+    # batch_size = config.batch_size
+    # gamma = config.gamma 
 
     env = OT2Env()
     env.reset()
 
-    model = PPO("MlpPolicy", env, learning_rate=learning_rate, n_steps=n_steps, batch_size=batch_size, gamma=gamma, verbose=1, tensorboard_log="./logs_final_hpt")
+    model = PPO("MlpPolicy", env, learning_rate=learning_rate, verbose=1, tensorboard_log="./logs_final_hpt")
 
     model.learn(total_timesteps=2_000_000, reset_num_timesteps=False)
 
     run.finish()
-
 
 wandb.agent(sweep_id, main)
