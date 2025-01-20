321c006fbe7685d5c700a3251a402797f668d837
RafSikkema235159 <235159@buas.nl>
2025-01-20 09:39:52 +0100

    changes

--- ot2-hpt.py
+++ ot2-hpt.py
@@ -3,7 +3,7 @@ from stable_baselines3 import PPO
 import os
 import wandb
 from stable_baselines3.common.callbacks import BaseCallback
-from ot2_gym_wrapper import OT2Env
+from ot2_gym_wrapper_final import OT2Env
 from clearml import Task
 import typing_extensions
 import tensorboard
@@ -12,7 +12,7 @@ from stable_baselines3.common.vec_env import DummyVecEnv
 
 os.environ['WANDB_API_KEY'] = 'e2424cc498b0bfce94a65893693a511407132b27'
 
-# task = Task.init(project_name="Mentor Group E/Group DMRM", task_name="ppo-hpt_raf")
+task = Task.init(project_name="Mentor Group E/Group DMRM", task_name="ppo-hpt_raf")
 
 # Define sweep config
 sweep_config = {
@@ -32,15 +32,11 @@ parameters_dict = Task.current_task().connect(sweep_config)
 
 #copy these lines exactly as they are
 #setting the base docker image
-# task.set_base_docker('deanis/2023y2b-rl:latest')
+task.set_base_docker('deanis/2023y2b-rl:latest')
 #setting the task to run remotely on the default queue
-# task.execute_remotely(queue_name="default")
+task.execute_remotely(queue_name="default")
 
 sweep_id = wandb.sweep(parameters_dict, project="sweep_for_weights")
-
-# Create and wrap the OT2 environment for compatibility
-def make_env():
-    return Monitor(OT2Env(render=False, max_steps=1000))
 
 def main(config=None):
     run = wandb.init(config, sync_tensorboard=True)
@@ -52,7 +48,7 @@ def main(config=None):
     batch_size = config.batch_size
     # gamma = config.gamma 
 
-    env = DummyVecEnv([make_env])
+    env = OT2Env(render=False)
 
     model = PPO("MlpPolicy", env, batch_size=batch_size, verbose=1, device="cuda", tensorboard_log="./logs_final_hpt")
 
